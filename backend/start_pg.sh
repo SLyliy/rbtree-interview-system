20b3c1f0bf223cf4e08ad53e6b02feba66530e74
SLyliy <1090861119@qq.com>
2026-03-08 20:30:31 +1100

    feat: start sh

--- backend/start_pg.sh
+++ backend/start_pg.sh
@@ -1,5 +1,5 @@
 # 新建一个 postgres 数据库容器
-docker run --name irb-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=irb@123456 -e POSTGRES_DB=irb_interview -p 5432:5432 -d postgres:latest
+docker run --name rbt-interview-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=rbt@123456 -p 5432:5432 -d postgres:latest
 
 # 启动现有的 postgres 数据库容器
-docker start irb-postgres
+docker start rbt-interview-postgres
